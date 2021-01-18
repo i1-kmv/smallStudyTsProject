3dc6be44ffb3f5a14aaf371dad92e4513867c6e3
i1-kmv <i1-kmv@mail.ru>
2021-01-18 19:20:08 +0300

    lesson 15

--- src/components/UseEffect/UseEffect.tsx
+++ src/components/UseEffect/UseEffect.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react'
+import React, {useEffect, useMemo, useState} from 'react'
 
 
 
@@ -9,13 +9,28 @@ function generateData() {
 
 export const Example1 = () => {
     console.log('Example1')
-
-   // const initValue = useMemo(generateData, [])
     const [counter, setCounter] = useState<any>(generateData)
     const changer = (state: number) => {
         return state+1
     }
 
+    useEffect(() => {
+        console.log('useEffect every render')
+        document.title = counter.toString()
+    })
+
+
+    useEffect(() => {
+        console.log('useEffect only first render(componentDidMount)')
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(() => {
+        console.log('useEffect only first render every counter change')
+        document.title = counter.toString()
+    }, [counter])
+
+   // const initValue = useMemo(generateData, [])
 
     return <>
         <button onClick={() => setCounter(changer)}>+</button>
